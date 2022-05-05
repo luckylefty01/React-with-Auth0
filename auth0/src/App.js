5756f4f8c47144cd0916d504d4a783636a726260
James Torres <james.torres01@gmail.com>
2022-05-05 11:44:03 -0400

    created component logout button, buttons working

--- auth0/src/App.js
+++ auth0/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
 
 function App() {
   return (
-   <LoginButton
+  <>
+    <LoginButton />
+    <LogoutButton />
+  </> 
   );
 }
 
